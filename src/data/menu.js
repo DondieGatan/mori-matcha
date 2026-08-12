971030d122ed8cecdabceb645f9aca34eb978fc1
DondieGatan <dondiegodwin@gmail.com>
2026-08-12 22:37:24 +0400

    Change Chocolate Cookies sugar unit back to Condensed Milk

--- src/data/menu.js
+++ src/data/menu.js
@@ -3,7 +3,7 @@ export const SUGAR_DATA = {
   'mango-matcha-bliss': { unit: 'Syrup', levels: { '25%': 5, '50%': 8, Regular: 12, Sweet: 20 } },
   'ube-matcha-latte': { unit: 'Condensed Milk', levels: { '25%': 5, '50%': 10, Regular: 15, Sweet: 25 } },
   'strawberry-matcha-latte': { unit: 'Syrup', levels: { '25%': 5, '50%': 8, Regular: 12, Sweet: 20 } },
-  'chocolate-cookies': { unit: 'Syrup', levels: { '25%': 10, '50%': 15, Regular: 20, Sweet: 25 } },
+  'chocolate-cookies': { unit: 'Condensed Milk', levels: { '25%': 10, '50%': 15, Regular: 20, Sweet: 25 } },
   'matcha-sea-salt': { unit: 'Syrup', levels: { '25%': 5, '50%': 8, Regular: 12, Sweet: 20 } },
 }
 
